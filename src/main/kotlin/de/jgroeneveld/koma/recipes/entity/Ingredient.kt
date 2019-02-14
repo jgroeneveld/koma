0f4bd2e3674f8a2899327141898ac4c6478ab721
Jaap Groeneveld <jaapgr@gmail.com>
2019-02-14 15:47:05 +0100

    closes #4

---
+++ src/main/kotlin/de/jgroeneveld/koma/recipes/entity/Ingredient.kt
@@ -0,0 +1,3 @@
+package de.jgroeneveld.koma.recipes.entity
+
+data class Ingredient(val Amount: Float, val quantity: Quantity, val name: String)
---
+++ src/main/kotlin/de/jgroeneveld/koma/recipes/entity/Quantity.kt
@@ -0,0 +1,11 @@
+package de.jgroeneveld.koma.recipes.entity
+
+enum class Quantity {
+    Kg,
+    G,
+    L,
+    Ml,
+    Tsp,
+    Tbsp,
+    Pieces
+}
